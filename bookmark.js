f37525444d0b3e58071edf4e0eda033a61d2091e
mmtrucefacts <93686164+mmtrucefacts@users.noreply.github.com>
2024-04-12 21:21:17 -0500

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,6 @@
+javascript: (function () {var url = "https://teacherease.net"; var urlobj = new
+window. URL (window. location.href); win = window.open(); win.document.body.style.margin = "0";
+win. document.body.style.height = "100h"; var iframe = win. document. createElement("iframe"); iframe.style.border =
+"none"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.margin = "0"; iframe.referrerpolicy
+= "no-referrer"; iframe.allow = "fullscreen"; iframe.sc = url. toString(); win.document.body.appenaChild (iframe);
+var script = win. document.createElement("script"); win.document.body.appendChild(script); 5)();
